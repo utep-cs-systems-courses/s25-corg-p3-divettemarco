e3963749f71cd625f8d380cc1db36325420cd5e4
divettemarco <divettemarcooutlook.com>
2025-10-15 17:11:15 -0600

    update with parts A, B, C in a switch statement

--- demos/1-led/main.c
+++ demos/1-led/main.c
@@ -4,9 +4,26 @@
 #include "led.h"
 
 int main(void) {
-  P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+  static int chosenPart = 2; // 0=A, 1=B, 2=C
 
+  switch(chosenPart){
+  case 0:
+    P1DIR |= LEDS; // specify all LED bits for output
+    P1OUT &= ~LED_GREEN; // turn off green
+    P1OUT |= LED_RED; // turn on red
+    break;
+  case 1:
+    P1DIR |= LEDS; // specify all LED bits for output
+    P1OUT |= LEDS; // turn on all LEDs
+    break;
+  case 2:
+    P1DIR |= LED_GREEN; // only specify green LED bit for output
+    P1OUT |= LEDS; // turn on green and fail to turn on red
+    break;
+  default:
+    break;
+  }
+  
   or_sr(0x18);		/* CPU off, GIE on */
+  return 0;
 }
